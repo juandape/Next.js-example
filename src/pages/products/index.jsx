9bf1e425fafd3f970e271a63e3ada20c85db1f6f
Juan David Peña <juandavidp76@hotmail.com>
2023-08-22 16:11:01 -0500

    docs: peticiones

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -3,10 +3,9 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const ProductsPage = ({products = []}) => {
+const ProductsPage = ({ products = [] }) => {
+  //peticion del lado del cliente
 
-//peticion del lado del cliente
-  
   // const [products, setProducts] = useState([]);
 
   // useEffect(() => {
@@ -46,10 +45,21 @@ const ProductsPage = ({products = []}) => {
 
 export default ProductsPage;
 
+//peticion del lado del servidor, pide datos al servidor en cada peticion
+// export async function getServerSideProps() {
+//   const url = 'http://localhost:3000/api/products';
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   return {
+//     props: {
+//       products: data,
+//     },
+//   };
+// }
 
-//peticion del lado del servidor
-export async function getServerSideProps() {
-  const url = 'http://localhost:3000/api/products';
+//peticion estatica, no pide datos al servidor, los datos se cargan en el build
+export async function getStaticProps() {
+  const url = 'https://fakestoreapi.com/products';
   const response = await fetch(url);
   const data = await response.json();
   return {
@@ -58,3 +68,6 @@ export async function getServerSideProps() {
     },
   };
 }
+
+//incremental static regeneration, pide datos al servidor,
+//los datos se cargan en el build y se actualizan cada cierto tiempo
